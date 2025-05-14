24c99ad0fefa0e6245bd224f509d73c2491a4c88
nazarenko-by <bodean56@ukr.net>
2025-05-14 13:58:53 -0400

    HA: add imgs

--- health_analysis.js
+++ health_analysis.js
@@ -81,7 +81,7 @@ function searchCondition() {
             const treatment = condition.treatment;
 
             resultDiv.innerHTML += `<h2>${condition.name}</h2>`;
-            resultDiv.innerHTML += `<img src="${condition.imagesrc}" alt="hjh">`;
+            resultDiv.innerHTML += `<img src="./img/${condition.imagesrc}" alt="hjh">`;
 
             resultDiv.innerHTML += `<p><strong>Symptoms:</strong> ${symptoms}</p>`;
             resultDiv.innerHTML += `<p><strong>Prevention:</strong> ${prevention}</p>`;
